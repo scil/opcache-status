da277d42abf0faf1c634255f4b9394dbaed78566
Andrés Hernández <andres@tonejito.org>
2017-03-08 12:08:20 -0600

    Removed data-sample.php

--- opcache.php
+++ opcache.php
@@ -17,8 +17,7 @@
 define('THOUSAND_SEPARATOR',true);
 
 if (!extension_loaded('Zend OPcache')) {
-    echo '<div style="background-color: #F2DEDE; color: #B94A48; padding: 1em;">You do not have the Zend OPcache extension loaded, sample data is being shown instead.</div>';
-    require 'data-sample.php';
+    die('<div style="background-color: #F2DEDE; color: #B94A48; padding: 1em;">You do not have the Zend OPcache extension loaded.</div>');
 }
 
 class OpCacheDataModel
